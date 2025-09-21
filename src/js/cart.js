4e96f6b105744be53787c22f5dceec0e1ee7e85b
futureniyi <futureniyi@yahoo.com>
2025-09-21 19:23:02 +0100

    week 2 - Add customizable alert to index.html

--- src/js/cart.js
+++ src/js/cart.js
@@ -26,3 +26,44 @@ function cartItemTemplate(item) {
 }
 
 renderCartContents();
+
+// import { getLocalStorage } from "./utils.mjs";
+
+// function renderCartContents() {
+//   const cartItems = getLocalStorage("so-cart");
+//   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
+//   document.querySelector(".product-list").innerHTML = htmlItems.join("");
+
+//   // Attach event listeners to remove buttons
+//   document.querySelectorAll('.remove-item').forEach(button => {
+//     button.addEventListener('click', (event) => {
+//       const itemId = event.target.dataset.id;
+//       let cartItems = getLocalStorage("so-cart");
+//       cartItems = cartItems.filter(item => item.Id !== itemId);
+//       localStorage.setItem("so-cart", JSON.stringify(cartItems));
+//       renderCartContents();
+//     });
+//   });
+// }
+
+// function cartItemTemplate(item) {
+//   const newItem = `<li class="cart-card divider">
+//   <a href="#" class="cart-card__image">
+//     <img
+//       src="${item.Image}"
+//       alt="${item.Name}"
+//     />
+//   </a>
+//   <a href="#">
+//     <h2 class="card__name">${item.Name}</h2>
+//   </a>
+//   <p class="cart-card__color">${item.Colors[0].ColorName}</p>
+//   <p class="cart-card__quantity">qty: 1</p>
+//   <p class="cart-card__price">$${item.FinalPrice}</p>
+//   <span class="remove-item" data-id="${item.Id}">X</span>
+// </li>`;
+
+//   return newItem;
+// }
+
+// renderCartContents();
--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
+import Alert from "./alert.js";
 
 const dataSource = new ProductData("tents");
 
@@ -8,3 +9,6 @@ const element = document.querySelector(".product-list");
 const productList = new ProductList("Tents", dataSource, element);
 
 productList.init();
+
+const alert = new Alert("./json/alerts.json");
+alert.init();
